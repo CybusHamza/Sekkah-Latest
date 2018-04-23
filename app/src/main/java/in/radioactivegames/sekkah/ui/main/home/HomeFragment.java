ecdef188fb26f9c9c583b9a20802fbf513149455
hamza <hamzabintariq123@yahoo.com>
2018-04-23 21:58:17 +0500

    localjson

--- app/src/main/java/in/radioactivegames/sekkah/ui/main/home/HomeFragment.java
+++ app/src/main/java/in/radioactivegames/sekkah/ui/main/home/HomeFragment.java
@@ -15,6 +15,8 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 import javax.inject.Inject;
@@ -32,43 +34,41 @@ import in.radioactivegames.sekkah.di.component.FragmentComponent;
 import in.radioactivegames.sekkah.ui.main.trainlist.TrainsFragment;
 import io.realm.Realm;
 
-public class HomeFragment extends BaseFragment implements HomeContract.View
-{
+public class HomeFragment extends BaseFragment implements HomeContract.View {
     private View mFragment;
-    @Inject HomePresenter mPresenter;
-     Realm realm;
-    @BindView(R.id.spnDeparture) Spinner spnDeparture;
-    @BindView(R.id.spnDestination) Spinner spnDestination;
+    @Inject
+    HomePresenter mPresenter;
+    Realm realm;
+    @BindView(R.id.spnDeparture)
+    Spinner spnDeparture;
+    @BindView(R.id.spnDestination)
+    Spinner spnDestination;
 
-    public static HomeFragment newInstance()
-    {
+    public static HomeFragment newInstance() {
         return new HomeFragment();
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState)
-    {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
 
     @Override
-    public void onInject(FragmentComponent fragmentComponent)
-    {
+    public void onInject(FragmentComponent fragmentComponent) {
         fragmentComponent.inject(this);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
-    {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         mFragment = inflater.inflate(R.layout.fragment_home, container, false);
         setUnbinder(ButterKnife.bind(this, mFragment));
         mPresenter.onAttach(this);
+        parsonJson(loadJSONFromAsset());
         return mFragment;
     }
 
     @Override
-    public void onViewCreated(View view, @Nullable Bundle savedInstanceState)
-    {
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mPresenter.getStationsData();
     }
@@ -77,7 +77,7 @@ public class HomeFragment extends BaseFragment implements HomeContract.View
     public void onResume() {
         String accessToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJVU0VSMTUxNTQzMzkxMjkyMiIsImd1aWQiOiI1YTUzYWZiOTBmODQyZjAwMTRiOTczMTYiLCJpYXQiOjE1MTU0OTc3NjR9.E1MRwZS3oDHTm0rm5XVD6Sq3Z9y_S1xSWotCOudm10s";
         WebSocketHelper webSocketHelper = new WebSocketHelper();
-        webSocketHelper.startScheduleTracking(accessToken,jsonCallback);
+        webSocketHelper.startScheduleTracking(accessToken, jsonCallback);
         super.onResume();
 
     }
@@ -86,81 +86,13 @@ public class HomeFragment extends BaseFragment implements HomeContract.View
         @Override
         public void onSuccess(final JSONObject jsonObject) {
 
-           realm = Realm.getDefaultInstance();
-           realm.beginTransaction();
-           realm.deleteAll();
-           realm.commitTransaction();
+            realm = Realm.getDefaultInstance();
+            realm.beginTransaction();
+            realm.deleteAll();
+            realm.commitTransaction();
 
+            parsonJson(jsonObject);
 
-
-            try {
-                JSONArray jsonArray = jsonObject.getJSONArray("schedule");
-
-                for (int i = 0 ; i <jsonArray.length();i++){
-
-                    final JSONObject jObject =  jsonArray.getJSONObject(i);
-
-                            realm.executeTransaction(new Realm.Transaction() {
-                                @Override
-                                public void execute(Realm realm) {
-
-                                    TrainPOJO trainPOJO = realm.createObject(TrainPOJO.class);
-
-                                    trainPOJO.setId(jObject.optString("_id",""));
-                                    trainPOJO.setNameen(jObject.optString("nameen",""));
-                                    trainPOJO.setNamear(jObject.optString("namear",""));
-                                    trainPOJO.setLat(jObject.optDouble("lat",0));
-                                    trainPOJO.setLng(jObject.optDouble("lng",0));
-                                    trainPOJO.setUpdatedAt(jObject.optString("updatedAt",""));
-                                    trainPOJO.setDelay(jObject.optString("delay",""));
-
-                                    try {
-                                        JSONArray array =  jObject.getJSONArray("stations");
-                                        JSONObject jsonObject1 = array.getJSONObject(0);
-                                        JSONObject jsonObject2 = jsonObject1.getJSONObject("stationId");
-                                        trainPOJO.setDepStation(jsonObject2.optString("nameen"));
-                                        trainPOJO.setGetDepStationtime(jsonObject1.optString("ts"));
-
-                                        JSONObject jsonObject3 = array.getJSONObject(array.length()-1);
-                                        JSONObject jsonObject4 = jsonObject3.getJSONObject("stationId");
-                                        trainPOJO.setFinalStation(jsonObject4.optString("nameen"));
-                                        trainPOJO.setFinalStationDepStationtime(jsonObject3.optString("ts"));
-
-                                    } catch (JSONException e) {
-                                        e.printStackTrace();
-                                    }
-
-                                }
-                            });
-
-
-                    JSONArray array =  jObject.getJSONArray("stations");
-
-                    for(int j =0 ; j<array.length();j++){
-                        final JSONObject staitionObj = array.getJSONObject(j);
-
-                        realm.executeTransaction(new Realm.Transaction() {
-                            @Override
-                            public void execute(Realm bgRealm) {
-
-                                StationPOJO stationPOJO = new StationPOJO();
-
-                                stationPOJO.setId(staitionObj.optString("_id",""));
-                                stationPOJO.setNameen(staitionObj.optString("namear",""));
-                                stationPOJO.setNamear(staitionObj.optString("nameen",""));
-                                stationPOJO.setLat(staitionObj.optDouble("lat",0));
-                                stationPOJO.setLng(staitionObj.optDouble("lng",0));
-                                stationPOJO.setDistance(staitionObj.optInt("distance",0));
-                            }
-                        });
-
-                            }
-
-
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
         }
 
         @Override
@@ -170,19 +102,111 @@ public class HomeFragment extends BaseFragment implements HomeContract.View
     };
 
     @Override
-    public void setStationsData(List<String> data)
-    {
+    public void setStationsData(List<String> data) {
         ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_spinner_item, data);
         spnDeparture.setAdapter(arrayAdapter);
         spnDestination.setAdapter(arrayAdapter);
     }
 
     @OnClick(R.id.btnTrains)
-    public void viewTrains()
-    {
+    public void viewTrains() {
         getFragmentManager().beginTransaction()
                 .add(R.id.frameMain, TrainsFragment.newInstance(), "TrainsFragment")
                 .addToBackStack("TrainsFragment")
                 .commit();
+    }
+
+
+    public JSONObject loadJSONFromAsset() {
+        String json = null;
+        JSONObject jsonObject = null;
+        try {
+            InputStream is = getResources().openRawResource(R.raw.shcedule);
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        try {
+            jsonObject = new JSONObject(json);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return jsonObject;
+    }
+
+    public void parsonJson(JSONObject jsonObject) {
+        try {
+            JSONArray jsonArray = jsonObject.getJSONArray("schedule");
+
+            for (int i = 0; i < jsonArray.length(); i++) {
+
+                final JSONObject jObject = jsonArray.getJSONObject(i);
+
+                realm.executeTransaction(new Realm.Transaction() {
+                    @Override
+                    public void execute(Realm realm) {
+
+                        TrainPOJO trainPOJO = realm.createObject(TrainPOJO.class);
+
+                        trainPOJO.setId(jObject.optString("_id", ""));
+                        trainPOJO.setNameen(jObject.optString("nameen", ""));
+                        trainPOJO.setNamear(jObject.optString("namear", ""));
+                        trainPOJO.setLat(jObject.optDouble("lat", 0));
+                        trainPOJO.setLng(jObject.optDouble("lng", 0));
+                        trainPOJO.setUpdatedAt(jObject.optString("updatedAt", ""));
+                        trainPOJO.setDelay(jObject.optString("delay", ""));
+
+                        try {
+                            JSONArray array = jObject.getJSONArray("stations");
+                            JSONObject jsonObject1 = array.getJSONObject(0);
+                            JSONObject jsonObject2 = jsonObject1.getJSONObject("stationId");
+                            trainPOJO.setDepStation(jsonObject2.optString("nameen"));
+                            trainPOJO.setGetDepStationtime(jsonObject1.optString("ts"));
+
+                            JSONObject jsonObject3 = array.getJSONObject(array.length() - 1);
+                            JSONObject jsonObject4 = jsonObject3.getJSONObject("stationId");
+                            trainPOJO.setFinalStation(jsonObject4.optString("nameen"));
+                            trainPOJO.setFinalStationDepStationtime(jsonObject3.optString("ts"));
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                });
+
+
+                JSONArray array = jObject.getJSONArray("stations");
+
+                for (int j = 0; j < array.length(); j++) {
+                    final JSONObject staitionObj = array.getJSONObject(j);
+
+                    realm.executeTransaction(new Realm.Transaction() {
+                        @Override
+                        public void execute(Realm bgRealm) {
+
+                            StationPOJO stationPOJO = new StationPOJO();
+
+                            stationPOJO.setId(staitionObj.optString("_id", ""));
+                            stationPOJO.setNameen(staitionObj.optString("namear", ""));
+                            stationPOJO.setNamear(staitionObj.optString("nameen", ""));
+                            stationPOJO.setLat(staitionObj.optDouble("lat", 0));
+                            stationPOJO.setLng(staitionObj.optDouble("lng", 0));
+                            stationPOJO.setDistance(staitionObj.optInt("distance", 0));
+                        }
+                    });
+
+                }
+
+
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
